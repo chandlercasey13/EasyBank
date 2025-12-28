2dd1dae53cc2dbf73ad9ed65891cac0070a1e7b1
chandlercasey13 <chandlercasey13@gmail.com>
2025-12-28 06:44:44 -0600

    Init

---
+++ src/main/java/org/chandlercasey/easybank/controllers/WelcomeController.java
@@ -0,0 +1,14 @@
+package org.chandlercasey.easybank.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Spring Application";
+    }
+}
+
