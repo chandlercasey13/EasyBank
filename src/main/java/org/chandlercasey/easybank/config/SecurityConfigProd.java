b4dcc81b82d336c1a3df54c97264659cfba1de74
chandlercasey13 <chandlercasey13@gmail.com>
2025-12-31 05:33:27 -0600

    Adds security config for prod env

--- src/main/java/org/chandlercasey/easybank/config/SecurityConfigProd.java
+++ src/main/java/org/chandlercasey/easybank/config/SecurityConfigProd.java
@@ -5,21 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@Profile("!prod")
-
-public class SecurityConfig {
+@Profile("prod")
+public class SecurityConfigProd {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         // this will not secure any routes at all
@@ -27,6 +22,7 @@ public class SecurityConfig {
 
         // this will secure all routes
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
+        http.redirectToHttps(withDefaults());
         http.csrf(csrfConfig -> csrfConfig.disable());
 
         http.authorizeHttpRequests((requests) -> requests
