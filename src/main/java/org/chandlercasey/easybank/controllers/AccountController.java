22f01e48eb10924d11d3c15f97abd7d2fc6960bf
chandlercasey13 <chandlercasey13@gmail.com>
2026-01-01 15:01:28 -0600

    Creates account controller

--- src/main/java/org/chandlercasey/easybank/controllers/AccountController.java
+++ src/main/java/org/chandlercasey/easybank/controllers/AccountController.java
@@ -1,14 +1,27 @@
 package org.chandlercasey.easybank.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.chandlercasey.easybank.entities.Accounts;
+import org.chandlercasey.easybank.repositories.AccountsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AccountController {
+    private final AccountsRepository accountsRepository;
+
 
     @GetMapping("/myAccount")
-    public String getAccountDetails(){
-        return "Account details from the DB";
+    public Accounts getAccountDetails(@RequestParam long id ){
+        Accounts accounts = accountsRepository.findByCustomerId(id);
+        if (accounts != null) {
+            return accounts;
+        } else {
+            return null;
+        }
     }
 }
 
